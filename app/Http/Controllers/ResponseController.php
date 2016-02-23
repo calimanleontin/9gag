25912b20c1af3b4690510b2e5ea66d0d9ea62969
calimanleontin <calimanleontin@gmail.com>
2016-02-23 14:19:26 +0200

    response rating backend added

--- app/Http/Controllers/ResponseController.php
+++ app/Http/Controllers/ResponseController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Comments;
+use App\ResponseRating;
 use App\Responses;
 use Illuminate\Http\Request;
 
@@ -43,4 +44,122 @@ class ResponseController extends Controller
         $response->delete();
         return redirect('/gag/'.$post->slug)->withMessage('Comment deleted');
     }
+
+    public function like($id)
+    {
+        $user = Auth::user();
+        if($user == null)
+            return redirect('/auth/login')->withErrors('You are not logged in');
+        $response = Responses::find($id);
+        if($response == null)
+            return redirect('/')->withErrors('Comment does not exist');
+        $comment = $response->comment;
+        if($comment == null)
+            return redirect('/')->withErrors('Internal error');
+        $post = $comment->post;
+        if($post == null)
+            return redirect('/')->withErrors('Internal error');
+
+        $rating = ResponseRating::where('response_id', $id)->where('user_id', $user->id)->first();
+
+
+        if($rating == null)
+        {
+            $response->votes = 1;
+            $response->save();
+            $rating = new ResponseRating();
+            $rating->likes = 1;
+            $rating->user_id = $user->id;
+            $rating->response_id = $comment->id;
+            $rating->save();
+            return redirect('/gag/'.$post->slug)->withMessage('Success');
+        }
+        if($rating->likes == 1)
+        {
+            $response->votes -= 1;
+            $response->save();
+            $rating->likes = 0;
+            $rating->dislikes = 0;
+            $rating->save();
+            return redirect('/gag/'.$post->slug)->withMessage('Success');
+        }
+        if($rating->likes == 0)
+        {
+            if($rating->dislikes == 1)
+            {
+                $response->votes += 2;
+                $response->save();
+            }
+            else
+            {
+                $response->votes += 1;
+                $response->save();
+            }
+            $rating->likes = 1;
+            $rating->dislikes= 0;
+            $rating->save();
+            return redirect('/gag/'.$post->slug)->withMessage('Success');
+        }
+    }
+
+    public function dislike($id)
+    {
+        $user = Auth::user();
+        if($user == null)
+            return redirect('/auth/login')->withErrors('You are not logged in');
+        $response = Responses::find($id);
+        if($response == null)
+            return redirect('/')->withErrors('Comment does not exist');
+        $comment = $response->comment;
+        if($comment == null)
+            return redirect('/')->withErrors('Internal error');
+        $post = $comment->post;
+        if($post == null)
+            return redirect('/')->withErrors('Internal error');
+
+        $rating = ResponseRating::where('response_id', $id)->where('user_id', $user->id)->first();
+
+        if($rating == null)
+        {
+            $response->votes = -1;
+            $response->save();
+
+            $rating = new ResponseRating();
+            $rating->comment_id = $comment->id;
+            $rating->user_id = $user->id;
+            $rating->dislikes = 1;
+            $rating->save();
+            return redirect('/gag/'.$post->slug)->withMessage('Success');
+        }
+
+        if($rating->dislikes == 1)
+        {
+            $response->votes += 1;
+            $response->save();
+
+            $rating->dislikes = 0;
+            $rating->likes = 0;
+            $rating->save();
+            return redirect('/gag/'.$post->slug)->withMessage('Success');
+        }
+
+        if($rating->dislikes == 0)
+        {
+            if($rating->likes == 1)
+            {
+                $response->votes -= 2;
+                $response->save();
+            }
+            else
+            {
+                $response->votes -= 1;
+                $response->save();
+            }
+            $rating->dislikes = 1;
+            $rating->likes = 0;
+            $rating->save();
+            return redirect('/gag/'.$post->slug)->withMessage('Success');
+        }
+    }
+
 }
